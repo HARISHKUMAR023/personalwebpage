0ff8a07ffc3d2b7674e4921d8ac633e8bc02dc77
Harishkumar <harishkumarsp023@gmail.com>
2021-12-12 18:21:42 +0530

    add index html image and some other new this in that

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const education = document.getElementById("education");
+function edu(){
 
+alert(" sorry for that,we not added this page, it will added soon,for more information contact us.")
+
+};
+education.addEventListener("click" , edu);
